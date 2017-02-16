c6d798bb691f58770ea4fa8f4da15047ddb21101
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-02-17 02:01:28 +0530

    fix : extract relevent filed from the json

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -4,13 +4,24 @@ import android.app.usage.UsageEvents;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.EventLog;
+import android.util.Log;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TsunamiAsyncTask task = new TsunamiAsyncTask();
+        task.execute();
 
 
     }
@@ -34,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
       update the screen to display information from the given link
      */
 
-    private void update(Words earthQuake) {
+    private void updateUi (Words earthQuake) {
         TextView eventTextView = (TextView) findViewById(R.id.eventFill_text_view);
         eventTextView.setText(earthQuake.getEvent());
 
@@ -86,14 +99,99 @@ public class MainActivity extends AppCompatActivity {
             return earthquake;
 
         }
+        /*
+        Make http reponse and get string response back
+         */
+        private String makeHttpRequest(URL url) throws IOException {
+            String jsonResponse="";
+            if(url== null){
+                return jsonResponse;
+            }
+            HttpURLConnection urlConnection =null;
+            InputStream inputStream =null;
+            try {
+                urlConnection= (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.connect();
+                if(urlConnection.getResponseCode()==200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                }
+                else{
+                    Log.e(LOG_TAG,"Error with response code "+urlConnection.getResponseCode());
+                }
+
+
+            }catch (Exception e){
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (inputStream != null) {
+                    // function must handle java.io.IOException here
+                    inputStream.close();
+                }
+            }
+
+
+            return jsonResponse;
+        }
+
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
+        }
 
         private Words extractFeatureFromJson(String jsonResponse) {
+            if(TextUtils.isEmpty(jsonResponse)){
+                return null;
+            }
+
+            try {
+                JSONObject baseJsonResponse = new JSONObject(jsonResponse);
+                JSONArray featureArray = baseJsonResponse.getJSONArray("features");
+
+                // If there are results in the features array
+                if (featureArray.length() > 0) {
+                    // Extract out the first feature (which is an earthquake)
+                    JSONObject firstFeature = featureArray.getJSONObject(0);
+                    JSONObject properties = firstFeature.getJSONObject("properties");
+
+                    // Extract out the title, time, and tsunami values
+                    String title = properties.getString("title");
+                    long time = properties.getLong("time");
+                    int tsunamiAlert = properties.getInt("tsunami");
+
+                    // Create a new {@link Event} object
+                    return new Words(title, time, tsunamiAlert);
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+            }
             return null;
         }
 
-        private String makeHttpRequest(URL url) {
-            return null;
+        @Override
+        protected void onPostExecute(Words earthquake) {
+            if (earthquake == null) {
+                return;
+            }
+
+            updateUi(earthquake);
         }
+
 
         private URL createUrl(String usgsRequestUrl) {
             URL url = null;
